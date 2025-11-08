aefb2c75f721c64bed4ed7cab91be1b0d7162013
Bleu24 <bryanreamill2403@gmail.com>
2025-11-08 15:41:49 +0800

    Replace CareerForm with NewCareerWizard in NewCareerPage component

--- src/app/recruiter-dashboard/careers/new-career/page.tsx
+++ src/app/recruiter-dashboard/careers/new-career/page.tsx
@@ -2,17 +2,18 @@
 
 import React from "react";
 import HeaderBar from "@/lib/PageComponent/HeaderBar";
-import CareerForm from "@/lib/components/CareerComponents/CareerForm";
+// Temporarily replace single form with segmented wizard
+import NewCareerWizard from "@/lib/components/CareerComponents/NewCareerWizard";
 
 export default function NewCareerPage() {
-    return (
-        <>
-        <HeaderBar activeLink="Careers" currentPage="Add new career" icon="la la-suitcase" />
-        <div className="container-fluid mt--7" style={{ paddingTop: "6rem" }}>
-          <div className="row">
-            <CareerForm formType="add" />
-          </div>
+  return (
+    <>
+      <HeaderBar activeLink="Careers" currentPage="Add new career" icon="la la-suitcase" />
+      <div className="container-fluid mt--7" style={{ paddingTop: "6rem" }}>
+        <div className="row">
+          <NewCareerWizard />
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 }
